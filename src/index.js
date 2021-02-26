c92e81d9c06129782e9451d6592372952afa0112
askorbezh <artyom.ireday@gmail.com>
2021-02-26 18:24:45 +0300

    feat:brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bc = str.split('');
+  for (let i = 0; i < bc.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if ( bc[i] === bracketsConfig[j][0] && bc[i + 1] === bracketsConfig[j][1] ) {
+        bc.splice (i, 2);
+        i = -1;
+        continue; 
+      }
+    }
+  }
+  if (bc.length === 0) {return true;}
+  else {return false;}
 }
